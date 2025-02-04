7806d3201fbbb0cf71fba3db724298e530af058b
STruAS1 <STruAS1@yandex.ru>
2025-02-04 07:51:22 +0300

    1

--- bot/bot.go
+++ bot/bot.go
@@ -35,7 +35,10 @@ func StartBot(cfg *config.Config) {
 				}
 			}
 		}
-
+		if update.Message != nil {
+			log.Println(update.Message.Chat.ID)
+			continue
+		}
 		if (update.Message != nil && update.Message.Chat.Type == "private") || (update.CallbackQuery != nil && update.CallbackQuery.Message.Chat.Type == "private") {
 			handlers.HandleUpdate(&update, ctx)
 		}
